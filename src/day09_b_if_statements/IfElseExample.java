e588f02d2975e2d4f8210eac1ba295f2afaaecde
Dina <515lincolnstreet@gmail.com>
2023-09-23 15:04:31 -0400

    my new commit for day 12

--- src/day09_b_if_statements/IfElseExample.java
+++ src/day09_b_if_statements/IfElseExample.java
@@ -1,57 +1,64 @@
 package day09_b_if_statements;
+
 public class IfElseExample {
-    public static void main(String[] args) {
-
     /*
-
         int score = 60;
-        if(score >= 75) {
-            System.out.println("Passing");
+        if (score >= 75){  // false
+            System.out.println("You passed the exam!");
         }
 
-        if(score < 75) {
-            System.out.println("Failing");
-        }
-
-    */
-
-        int score = 50;
-        if(score >= 75){
-            System.out.println("Passing");
-        } else {
-            System.out.println("Failing");
-        }
-
-    /*
-        int year = 2021;
-        boolean lockdown = year == 2020 || year == 2021;
-
-        if(lockdown){
-            System.out.println("Stay at home");
-            System.out.println("Practice java");
-            System.out.println("Wear mask");
-        }
-
-        if(!lockdown){  // !false -> true
-            System.out.println("Party");
-            System.out.println("Still practice java");
-            System.out.println("Maybe travel");
+        if (score < 75) {  // true
+            System.out.println("You failed!");
         }
      */
+    public static void main(String[] args) {
 
-        int year = 2021;
-        boolean lockdown = year == 2020 || year == 2021;
+        int score = 60;
 
-        if(lockdown) {
-            System.out.println("Stay at home");
-            System.out.println("Practice java");
-            System.out.println("Wear mask");
+        if (score >= 75) {  // check if this line TRUE, execute the if body,
+
+            System.out.println("Passing!");
+
         } else {
-            System.out.println("Party");
-            System.out.println("Still practice java");
-            System.out.println("Maybe travel");
+            System.out.println("Failing!");
         }
+
+
+
+
+
+        /*
+                int year = 2023;
+        boolean isLockdown = year == 2020 || year == 2021;
+
+        if (isLockdown) {
+            System.out.println("Stay at home");
+            System.out.println("Study Java");
+            System.out.println("When you study put your mask on");
+        }
+
+        // any single if statement does NOT depend on another statement
+        if (!isLockdown) {
+            System.out.println("Party");
+            System.out.println("This is not a normal party. It is a Java Party");
+            System.out.println("Travel");
+        }
+         */
+        System.out.println();
+        int year = 2024;
+        boolean isLockdown = year == 2020 || year == 2021;
+
+        //  false
+        if (isLockdown) {
+            System.out.println("Stay at home");
+            System.out.println("Study Java");
+            System.out.println("When you study put your mask on");
+        }else {
+            System.out.println("Party");
+            System.out.println("This is not a normal party. It is a Java Party");
+            System.out.println("Travel");
+        }
+
     }
+
 }
-
-
