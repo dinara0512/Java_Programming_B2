8705ccb5be52ffd82895a807edf818d1038cda26
Dina <515lincolnstreet@gmail.com>
2023-09-30 15:10:51 -0400

    more Strings Methods2

---
+++ src/day15_string/MergeString.java
@@ -0,0 +1,39 @@
+package day15_string;
+
+public class MergeString {
+
+//import java.util.Scanner;
+        public static void main(String[] args) {
+
+
+        /*
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter word1: ");
+        String a = input.next();
+        System.out.print("Enter word2: ");
+        String b = input.next();
+         */
+            String a = "abc";
+            String b = "xyz";
+
+            // ----- > axbycz
+
+            String merged = "";
+            merged += a.charAt(0);     //this is the same thing ---- > merged = merged + a.charAt(0);
+            merged += b.charAt(0);
+
+
+            merged += a.charAt(1);
+            merged += b.charAt(1);
+
+
+            merged += a.charAt(2);
+            merged += b.charAt(2);
+
+            System.out.println(merged);
+
+
+        }
+
+    }
+
