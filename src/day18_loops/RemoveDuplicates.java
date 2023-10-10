7d5dee6c4ecdd4fa50d62dd840c92aa38d64ecde
Dina <515lincolnstreet@gmail.com>
2023-10-10 08:43:31 -0400

    day 18 loops

---
+++ src/day18_loops/RemoveDuplicates.java
@@ -0,0 +1,30 @@
+package day18_loops;
+
+public class RemoveDuplicates {
+
+    /*
+        intput: abcabcd
+        output: abcd
+     */
+
+        public static void main(String[] args) {
+
+            String original = "abcabcabd";
+            String unique = "";
+
+        /*
+            loop through to get each character
+                put that character into another container if that container does not have it
+         */
+
+            for (int i = 0; i < original.length(); i++) {
+                if (!unique.contains("" + original.charAt(i))) {
+                    unique += original.charAt(i);
+                }
+            }
+
+            System.out.println("With duplicates: " + original);
+            System.out.println("With NO duplicates: " + unique);
+
+        }
+    }
