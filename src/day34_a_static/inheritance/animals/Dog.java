ab1ef4ff78e85950c46a5142a8096286cfc60a76
Dina <515lincolnstreet@gmail.com>
2023-11-16 07:25:14 -0500

    new class

---
+++ src/day34_a_static/inheritance/animals/Dog.java
@@ -0,0 +1,15 @@
+package day34_a_static.inheritance.animals;
+
+public class Dog {
+    public String species;
+    public int numberOfLegs;
+    boolean isDomestic;
+
+    public void bark () {
+        System.out.println("Dog is barking.");
+    }
+
+    public void walk() {
+    }
+}
+
